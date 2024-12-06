0eae27b64928e2619f69dcc206d32ac9a9e0573f
lohithvp-robosoft <lohith.vp@robosoftin.com>
2024-12-06 17:34:03 +0530

    first commit

---
+++ src/main/java/com/example/empoyeewith0Auth2/Empoyeewith0Auth2Application.java
@@ -0,0 +1,13 @@
+package com.example.empoyeewith0Auth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Empoyeewith0Auth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Empoyeewith0Auth2Application.class, args);
+	}
+
+}
